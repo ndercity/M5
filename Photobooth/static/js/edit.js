3ae5fdbd9c33f0b7a69bfda2d32fcebf16ede69b
Nathaniel Ephraim Canilang <nathcanilang@gmail.com>
2025-04-13 14:25:10 +0800

    added git ignore para hindi na masira ang aking repo

--- Photobooth/static/js/edit.js
+++ Photobooth/static/js/edit.js
@@ -1,5 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("edit selection page loaded.");
 
-    const imagePreview = document.getElementById('image-preview');
+    const modeButtons = document.querySelectorAll('.mode-selection-buttons')
+    const modeFilter = document.querySelectorAll('.mode-set')
+    const imageFeed = document.getElementById("image-feed");
+
+    //test functions
+    function startCamera(){
+        imageFeed.src = "/video_feed?" + new Date().getTime();
+    }
+
+    function stopCamera(){
+        videoFeed.src = "";
+        fetch("/stop_camera");
+    }
+
+    window.addEventListener("load", startCamera);
+    window.addEventListener("beforeunload", stopCamera);
+    //end of test functions
+
+
+
+    modeButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modeButtons.forEach(b => b.classList.remove('active'));  
+            btn.classList.add('active'); 
+
+            modeFilter.forEach(set=>set.classList.remove('visible'));
+            const target = btn.getAttribute('data-target');
+            document.getElementById(target).classList.add('visible');
+        });
+    });
 });
